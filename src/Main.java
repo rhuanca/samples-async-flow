5c3af20c3a33233a672ffbfc113628715d3b3997
rhuanca <rhuanca@gmail.com>
2016-08-07 20:01:44 -0400

    renamed Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class AsyncFlow {
+public class Main {
     
     private final static int MAX = 500;
 
